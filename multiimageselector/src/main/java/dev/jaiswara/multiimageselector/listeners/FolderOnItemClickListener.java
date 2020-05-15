91fbcba274d3f767b4783c38cc4586bf88b16a95
Ravi Jaiswara <ravijaiswara92@gmail.com>
2020-05-16 00:19:51 +0530

    Initial commit

---
+++ multiimageselector/src/main/java/dev/jaiswara/multiimageselector/listeners/FolderOnItemClickListener.java
@@ -0,0 +1,7 @@
+package dev.jaiswara.multiimageselector.listeners;
+
+import dev.jaiswara.multiimageselector.models.Folder;
+
+public interface FolderOnItemClickListener {
+    void folderOnItemClick(int position, Folder folder);
+}
---
+++ multiimageselector/src/main/java/dev/jaiswara/multiimageselector/listeners/ImageOnItemClickListener.java
@@ -0,0 +1,7 @@
+package dev.jaiswara.multiimageselector.listeners;
+
+import dev.jaiswara.multiimageselector.models.Image;
+
+public interface ImageOnItemClickListener {
+    void imageOnItemClick(int position, Image folder);
+}
